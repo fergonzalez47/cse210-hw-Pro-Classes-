35507592a97e9321a9117a541c48a81934af83c5
Fernando Gonzalez <fernandogonzalezvaldz@gmail.com>
2024-02-07 12:29:36 -0300

    learning 05 ready and done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine("*******");
+
+        Square  square = new Square("blue", 2);
+        // square.GetColor();
+        // square.GetArea();
+        Console.WriteLine($"{square.GetColor()}");
+        Console.WriteLine($"{square.GetArea()}");
+        Circle circle = new Circle("red", 3);
+        Rectangle rectangle = new Rectangle("black", 2, 4);
+
+
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(circle);
+        shapes.Add(rectangle);
+
+
+        foreach (var shape in shapes)
+        {
+
+            Console.WriteLine($"{shape.GetColor()}");
+            Console.WriteLine($"{shape.GetArea()}");
+        }
     }
+
+
 }
